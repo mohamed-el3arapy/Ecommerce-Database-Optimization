e16a9d9be1fd2f1d30f77a9a2aa352953c56db61
mohamed-el3arapy <mohamedel3arapy@gmail.com>
2026-06-13 01:28:04 +0300

    Update query_3.sql

--- Queries/query_3.sql
+++ Queries/query_3.sql
@@ -1,6 +1,6 @@
-EXPLAIN ANALYZE
-SELECT c.customer_id, c.first_name, c.last_name, SUM(o.total_amount) AS Total_Spent 
-FROM customer c 
-JOIN orders o ON c.customer_id = o.customer_id 
-GROUP BY c.customer_id, c.first_name, c.last_name 
-ORDER BY Total_Spent DESC;
+Explain analyze
+SELECT o.order_id, o.order_date, o.total_amount, c.first_name, c.last_name
+FROM orders o
+JOIN customer c ON o.customer_id = c.customer_id
+ORDER BY o.order_date DESC
+LIMIT 1000;
